75d8af01bcfe72a1c8f00917694ebf5028f5ff9c
Echoxu233 <767293803@qq.com>
2018-06-30 18:10:45 +0800

    changes

--- haircut/models.py
+++ haircut/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Order(models.Model):
+     barber = models.CharField(max_length=180, null=True, blank=True)
+     customer = models.CharField(max_length=180, null=True, blank=True)
+     time = models.CharField(max_length=180, null=True, blank=True)
--- haircut/views.py
+++ haircut/views.py
@@ -1,13 +1,25 @@
-from django.shortcuts import render
-
-# Create your views here.
-
 from django.shortcuts import render 
+from .models import Order
 
 def home_page(request): 
 
-	if request.method == "GET": 
-		return render(request, 'haircut/home_page.html')  
+    if request.method == "GET": 
+        return render(request, 'haircut/home_page.html')  
 
-	if request.method == "POST": 
-		return render(request, 'haircut/home_page.html')
+    if request.method == "POST": 
+        our_barber = request.POST.get("barber")
+        our_customer = request.POST.get("customer")
+        our_time = request.POST.get("time")
+        order = Order.objects.create(
+            barber=our_barber,
+            customer=our_customer,
+            time=our_time
+        )
+        all_orders = Order.objects.all()
+        context = {'orders': all_orders}
+        
+        return render(request, 'haircut/home_page.html', context)
+
+    all_orders = Order.objects.all()
+    context = {'orders': all_orders}
+    return render(request, 'haircut/home_page.html', context)
